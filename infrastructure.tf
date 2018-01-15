4d6d91e2e9b87cf546b529925ff6f68b1320f91c
Donald Rauscher <donald.rauscher@gmail.com>
2018-01-15 11:33:31 -0600

    Add entries for Postgres

--- infrastructure.tf
+++ infrastructure.tf
@@ -4,9 +4,30 @@ provider "google" {
   region = "us-central1"
 }
 
-resource "google_compute_disk" "default" {
+resource "google_compute_disk" "redash-redis-disk" {
   name  = "redash-redis-disk"
   type  = "pd-ssd"
   size = "200"
   zone  = "us-central1-f"
+}
+
+resource "google_sql_database_instance" "redash-db" {
+  name = "redash-db"
+  database_version = "POSTGRES_9_6"
+  region  = "us-central1"
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database" "redash-schema" {
+  name = "redash"
+  instance = "${google_sql_database_instance.redash-db.name}"
+}
+
+resource "google_sql_user" "proxyuser" {
+  name = "proxyuser"
+  instance = "${google_sql_database_instance.redash-db.name}"
+  host = "cloudsqlproxy~%"
+  password = "hsader"
 }
